05630d5c729d270dbe8894001de3fd73a2e8808c
Tec Tale <samuelrmeyer@gmail.com>
2018-09-12 23:08:56 -0400

    Check Time

--- routes/processpay.js
+++ routes/processpay.js
@@ -6,24 +6,31 @@ const payRouter = express.Router();
 
 payRouter.use(bodyParser.json());
 
-payRouter
-  .route("/")
-  .post((req, res) => {
-    const stripetoken = req.body.stripetoken;
-    const amountpayable = req.body.amount;
-    const charge = stripe.charges
-      .create({
-        amount: amountpayable,
-        currency: "cad",
-        description: "Sample transaction",
-        source: stripetoken
-      })
-      .then(charge => {
-        res.send({ success: true });
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-  });
+payRouter.route("/").post((req, res) => {
+  const stripetoken = req.body.stripetoken;
+  const amountpayable = req.body.amount;
+  const charge = stripe.charges
+    .create({
+      amount: amountpayable,
+      currency: "cad",
+      description: "Sample transaction",
+      source: stripetoken
+    })
+    .then(charge => {
+      res.send({ success: true });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
+payRouter.route("/check").post((req, res) => {
+  let currentHour = new Date().getHours();
+  if (currentHour > 10 || currentHour < 3) {
+    res.send({ message: "success" });
+  } else {
+    res.send({ message: "Store is Currently Closed" });
+  }
+});
 
 module.exports = payRouter;
